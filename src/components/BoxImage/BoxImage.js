bbc7407cd0b59f67532e666a43a12f42b4f0f4bb
Paulo <paulobiscocho@gmail.com>
2021-09-24 03:51:50 +0800

    Create Floating Box Image Component

---
+++ src/components/BoxImage/BoxImage.js
@@ -0,0 +1,23 @@
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+export default function BoxImage() {
+    const matches = useMediaQuery((theme) => theme.breakpoints.up('sm'));
+
+    const top = (matches) ? '60vh' : '85vh';
+    const width = (matches) ? '50vw' : '100vw';
+
+
+    return (
+        <Box component='img' src="/taxi.png" alt="taxi"
+            sx={{
+                width: width,
+                position: 'absolute',
+                top: top,
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+            }}
+        />
+
+    )
+}
