d6ea25dabd78d9033f5012ba88f2c7dcae831e21
Paulo <paulobiscocho@gmail.com>
2021-09-25 15:41:53 +0800

    Create Fake Features Data

---
+++ src/fakedata/features.js
@@ -0,0 +1,33 @@
+import StarIcon from '@mui/icons-material/Star';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+
+const features = [
+    {
+        id: 1,
+        title: 'HOME PICKUP',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        icon: StarIcon,
+    },
+    {
+        id: 2,
+        title: 'FAST BOOKING',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        icon: HourglassBottomIcon,
+    },
+    {
+        id: 3,
+        title: 'BONUSES FOR RIDE',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        icon: SettingsIcon,
+    },
+    {
+        id: 4,
+        title: 'GPS SEARCHING',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        icon: PriceCheckIcon,
+    }
+]
+
+export default features;
