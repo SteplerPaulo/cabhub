ccc59a83ee00ad0286bdf96419dfe9774dd5a03b
Paulo <paulobiscocho@gmail.com>
2021-09-25 15:42:22 +0800

    Create Fake Testimonial Data

---
+++ src/fakedata/testimonials.js
@@ -0,0 +1,19 @@
+
+const testimonials = [
+    {
+        id: 1,
+        name: 'EILEEN JOHNSON',
+        title:'EVENTO CEO',
+        description: 'Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.',
+        img: '/portrait.jpg',
+    },
+    {
+        id: 2,
+        name: 'AUBRIELLA WILLIAMS',
+        title:'HR MANAGER',
+        description: 'Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.',
+        img: '/portrait-1.jpg',
+    },
+]
+
+export default testimonials;
