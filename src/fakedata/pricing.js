c62527a5a21749be7831c380e375418b07973744
Paulo <paulobiscocho@gmail.com>
2021-09-25 15:42:11 +0800

    Create Fake Pricing Data

---
+++ src/fakedata/pricing.js
@@ -0,0 +1,25 @@
+const pricing = [
+    {
+        id: 1,
+        title:'ECONOMY CLASS',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        price:'$1.5/MI',
+        img:'taxi.png',
+    },
+    {
+        id: 2,
+        title:'STANDARD CLASS',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        price:'$1.5/MI',
+        img:'taxi.png',
+    },
+    {
+        id: 3,
+        title:'BUSINESS CLASS',
+        description: 'Nulla quis lorem ut libero malesuada feugiat. Donec rutrum congue leo eget malesuada.',
+        price:'$1.5/MI',
+        img:'taxi.png',
+    }
+]
+
+export default pricing;
