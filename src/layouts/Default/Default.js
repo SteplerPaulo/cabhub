20f4ffd4b1d6952cf55b7a0a033b75bd643cd3cc
Paulo <paulobiscocho@gmail.com>
2021-09-25 15:42:36 +0800

    Update Default Layout

--- src/layouts/Default/Default.js
+++ src/layouts/Default/Default.js
@@ -2,15 +2,28 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-//import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 
 
+
+
 import Booking from "components/Forms/Booking";
-import BoxImage from "components/BoxImage/BoxImage";
+import BoxImage from "components/Box/BoxImage";
 import Container from "components/Container/Container";
 import Navigation from "components/Navigation/Navigation";
+
+import Features from "components/Container/Features";
+import DownloadApp from "components/Container/DownloadApp";
+import Testimonials from "components/Container/Testimonials";
+import CallToAction from "components/Container/CallToAction";
+import Footer from "components/Container/Footer";
+
+
+
+import pricing from "fakedata/pricing";
+
+
 
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -23,6 +36,10 @@ const theme = createTheme({
     },
     secondary: {
       main: '#000000',
+    },
+    text: {
+      primary: '#ffc71a',
+      secondary: '#000000'
     }
   },
 });
@@ -30,24 +47,22 @@ const theme = createTheme({
 
 function DefaultLayout() {
 
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BoxImage />
 
+
       <Grid container
         style={{
-          backgroundColor: '#0d0d0d',
           paddingBottom: 250,
-          backgroundImage: `url(${"/background.png"})`,
+          background: `url(${"/background.png"}) rgba(0, 0, 0, 0.70)`,
+          backgroundBlendMode: 'multiply',
           backgroundSize: 'cover',
-          backgroundRepeat: 'no - repeat',
-          backgroundPosition: 'center center'
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'bottom center'
         }}>
-
-
-
+          
         <Navigation />
 
 
@@ -64,27 +79,28 @@ function DefaultLayout() {
             </Typography>
           </Box>
         </Grid>
+
       </Grid>
 
 
 
 
       <Grid container style={{ backgroundColor: '#ffc71a' }} p={{ xs: 4, md: 15 }} >
-        <Grid item md={6} xs={12} pb={2}>
-            <Typography variant="h6" component="div">
-              BEST IN CITY
-            </Typography>
-            <Typography variant="h4" component="div">
-              TRUSTED CAB SERVICE
-            </Typography>
-            <Typography variant="h5" component="div">
-              IN NATIONAL CAPITAL REGION
-            </Typography>
-            <Typography variant="p" component="p" pt={2} pb={2} align='justify'>
-              Nostrum ipsam tellus nisl, vel elit eligendi, arcu consectetur praesent aliquet atque praesentium necessitatibus repellat ipsam wisi quae! Montes natoque! Iusto per! Porta ridiculus ut eu gravida numquam mollit integer vestibulum sem. Iusto etiam sapiente et, ab nulla beatae, et eleifend eleifend quae mauris? Netus facilisis? Praesentium cumque, expedita convallis quam voluptatibus fuga, quos, anim minima, magnis, per lectus justo, convallis urna, vel! Aute hymenaeos ornare magnam doloribus, sed vero dictumst, incidunt, error ullam, adipisci ullamco? Eros vehicula, venenatis maecenas, minim aliquid mollit taciti. Dolorem officiis hac soluta? Erat ut. Inventore? Ipsa. Quos, inventore tenetur tempus, auctor, congue, suscipit veritatis.
-            </Typography>
-            <Button variant="contained" color="secondary">READ MORE</Button>
-         
+        <Grid item md={6} xs={12} pb={2} color="text.secondary">
+          <Typography variant="h6" component="div">
+            BEST IN CITY
+          </Typography>
+          <Typography variant="h4" component="div">
+            TRUSTED CAB SERVICE
+          </Typography>
+          <Typography variant="h5" component="div">
+            IN NATIONAL CAPITAL REGION
+          </Typography>
+          <Typography variant="p" component="p" pt={2} pb={2} align='justify'>
+            Nostrum ipsam tellus nisl, vel elit eligendi, arcu consectetur praesent aliquet atque praesentium necessitatibus repellat ipsam wisi quae! Montes natoque! Iusto per! Porta ridiculus ut eu gravida numquam mollit integer vestibulum sem. Iusto etiam sapiente et, ab nulla beatae, et eleifend eleifend quae mauris? Netus facilisis? Praesentium cumque, expedita convallis quam voluptatibus fuga, quos, anim minima, magnis, per lectus justo, convallis urna, vel! Aute hymenaeos ornare magnam doloribus, sed vero dictumst, incidunt, error ullam, adipisci ullamco? Eros vehicula, venenatis maecenas, minim aliquid mollit taciti. Dolorem officiis hac soluta? Erat ut. Inventore? Ipsa. Quos, inventore tenetur tempus, auctor, congue, suscipit veritatis.
+          </Typography>
+          <Button variant="contained" color="secondary">READ MORE</Button>
+
         </Grid>
         <Grid item md={6} xs={12} container direction="row" justifyContent="center" alignItems="center">
           <Booking />
@@ -93,10 +109,13 @@ function DefaultLayout() {
       </Grid>
 
 
+      <Container  {...{ color: 'black', backgroundColor: 'white', pricing }} />
 
-      <Container  {...{ color: 'black', backgroundColor: 'white' }} />
-
-
+      <Features />
+      <DownloadApp />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
 
     </ThemeProvider >
   );
