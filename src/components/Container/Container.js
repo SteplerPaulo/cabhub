dd28d16b56134690e0e57cdd03730d5ed6cb23f5
Paulo <paulobiscocho@gmail.com>
2021-09-25 15:40:17 +0800

    Update Container Component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,40 @@
-import Grid from '@mui/material/Grid';
+
+import Grid from "@mui/material/Grid";
+import Card from "components/Card/MediaCard";
+
+import Typography from "@mui/material/Typography";
+
+
 
 export default function Container(props) {
-    const { backgroundColor, color } = props;
-    console.log(props)
+    const { backgroundColor, color, pricing } = props;
+    console.log(pricing)
+
+
     return (
-        <Grid container spacing={2} style={{ color:  color , backgroundColor: backgroundColor , paddingTop: '100px',paddingBottom: '100px' }} >
-            
+
+        <Grid container direction="row"
+            justifyContent="center" alignItems="center" spacing={2}
+            pl={{ "xs": 1, "md": 15 }}
+            pr={{ "xs": 1, "md": 15 }}
+            pb={5} pt={5}
+            style={{
+                color: color, backgroundColor: backgroundColor
+            }}>
+
+
+            <Grid item xs={12}>
+                <Typography component="h2" variant="h5" color="primary" align="center" >OUR</Typography>
+                <Typography component="h2" variant="h5" color="common.black" align="center" fontWeight="bold" >TARIFFS</Typography>
+            </Grid>
+
+            {pricing.map((item) => (
+                <Grid item xs={6} md={3} key={item.id}>
+                    <Card {...item} />
+                </Grid>
+            ))}
+
         </Grid>
+
     );
 }
